bc2f3b85db4284b2d3953e6cad48da623a70bbf5
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-05 10:22:37 -0400

    token setup

--- src/main/java/com/revature/pokemon/services/TokenService.java
+++ src/main/java/com/revature/pokemon/services/TokenService.java
@@ -1,5 +1,24 @@
 package com.revature.pokemon.services;
 
+import java.util.Date;
+
+import com.revature.pokemon.dtos.responses.Principal;
+
+import io.jsonwebtoken.Jwts;
+
 public class TokenService {
     
+    public String generateJWT(Principal principal){
+        String jws = Jwts.builder()
+            .setIssuer("Pokemon")
+            .setSubject(principal.getRole().getName())
+            .setAudience(principal.getUsername())
+            .setExpiration(new Date(System.currentTimeMillis() + 60*60*1000)) //a java.util.Date
+            .setIssuedAt(new Date()) // for example, now
+            .setId(principal.getId()) //just an example id
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+
+        return jws;
+    }
 }
