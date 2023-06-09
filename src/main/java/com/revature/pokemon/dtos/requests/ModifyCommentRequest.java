020485a3f2c216a6dcd9b990b77c0709a549e7c7
minhe7735 <61667570+minhe7735@users.noreply.github.com>
2023-06-09 17:22:48 -0400

    comment setup

---
+++ src/main/java/com/revature/pokemon/dtos/requests/ModifyCommentRequest.java
@@ -0,0 +1,18 @@
+package com.revature.pokemon.dtos.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ModifyCommentRequest {
+    private String comment;
+    private String token;
+    private String userId;
+    private String postId;
+    private String commentId;
+}
